e38fa1724e8f43edaffcc15f3b3eef697a75574f
Xarpagon <slawomirbs@gmail.com>
2022-08-31 11:17:02 +0200

    Indents in leon2 fixed.

--- src/leon2.c
+++ src/leon2.c
@@ -48,24 +48,24 @@
 
 /* LEON2 APB register addresses */
 
-#define IRQCTRL_IPR	0x094
-#define IRQCTRL_IMR	0x090
-#define IRQCTRL_ICR	0x09C
-#define IRQCTRL_IFR	0x098
+#define IRQCTRL_IPR   0x094
+#define IRQCTRL_IMR   0x090
+#define IRQCTRL_ICR   0x09C
+#define IRQCTRL_IFR   0x098
 #define TIMER_SCALER	0x060
 #define TIMER_SCLOAD	0x064
 #define LEON2_CONFIG	0x024
 #define TIMER_TIMER1	0x040
 #define TIMER_RELOAD1	0x044
-#define TIMER_CTRL1	0x048
+#define TIMER_CTRL1   0x048
 #define TIMER_TIMER2	0x050
 #define TIMER_RELOAD2	0x054
-#define TIMER_CTRL2	0x058
-#define CACHE_CTRL	0x014
-#define POWER_DOWN	0x018
+#define TIMER_CTRL2   0x058
+#define CACHE_CTRL    0x014
+#define POWER_DOWN    0x018
 
-#define APBUART_RXTX	0x070
-#define APBUART_STATUS	0x074
+#define APBUART_RXTX    0x070
+#define APBUART_STATUS  0x074
 
 /* Size of UART buffers (bytes).  */
 #define UARTBUF	1024
